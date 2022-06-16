84e9a057a4147595819b272258430262f59658d2
Gabriel  Mastrangelo Canuto <63468823+Gabrielmastrangelo@users.noreply.github.com>
2022-06-16 09:16:42 -0800

    Update the Table reactively, new Update button
    Now, we create a function to read the file, and we defined a reactive object (data),
    that stores call the function every time there is a interaction with the buttons.
    OBS: Does not reacts to interactions with the table

--- app.R
+++ app.R
@@ -3,7 +3,7 @@ library("readxl")
 library(lubridate)
 
 # Function that gets the table that is updated by the python file.
-get_table <- function() {
+get_table <- function(input_) {
   #Ignore the index column
   raw_data <- read_excel("data.xlsx", range = cell_cols(c("B","AE")))
   
@@ -12,8 +12,10 @@ get_table <- function() {
   #I was told that all data they need is between 48 hours
   datetime_threshold <- datetime_now + hours(48)
   
-  raw_data[raw_data[['Order Time']] <= datetime_threshold, ]
+  return(raw_data[raw_data[['Order Time']] <= datetime_threshold, ])
 }
+
+variable_names <- c('Status', 'Job PO', 'Job #', 'Vessel Name', 'Order Time', 'From', 'To', 'Agency', 'Tug From', 'Tug To', 'Pilot #1', 'Pilot #2', 'Job On Time', 'ETD', 'ETA', 'IMO', 'Call Sign', 'Vessel Type/Dock', 'Vessel Flag', 'LOA', 'GRT', 'Actual Draft', 'S. Draft', 'Beam', 'Max. Beam', 'Speed', 'Hellicopter from', 'Hellicopter to', 'Job Notes', 'Vessel Notes')
 
 
 ui <- fluidPage(
@@ -26,30 +28,33 @@ ui <- fluidPage(
       selectInput("category", "Choose a category", 
                   c('Saam Towage', 'Seaspan', 'Group Ocean', 'Monitored Vessels')),
       selectInput(
-        "variables", "Choose the columns?", names(data),
+        "variables", "Choose the columns?", variable_names,
         multiple = TRUE,
         selected = c('Vessel Name', 'Order Time', 'From', 'To', 'Agency', 'Tug From', 'Tug To', 'Job On Time')
         #width = 4
-        )
-      
+        ),
+      actionButton("update", "Update Table")
     ),
     
     mainPanel(
-      dataTableOutput("dynamic"),
-      textOutput("text")
-    )
-    
+      dataTableOutput("dynamic")
+    ),
   )
 )
 
 
-
 server <- function(input, output) {
   
-  data <- reactive(get_table())
+  # The table reads the file again for any interaction with the buttons
+  data <- reactive({
+    input$category
+    input$update
+    input$variables
+    get_table()
+    })
   
   output$dynamic <- renderDataTable(data()[input$variables], options = list(pageLength = 20))
-  
+
 }
 
 shinyApp(ui = ui, server = server)
